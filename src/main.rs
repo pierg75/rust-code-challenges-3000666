ce8b4668cb254fe42da89816f824bf5d135b40bd
Pierguido Lambri <plambri@redhat.com>
2025-08-01 15:11:25 +0100

    Added solution for exercise 05
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -27,29 +27,135 @@ fn print_morse_code(code: &Message) {
     for letter in code.iter() {
         for pulse in letter.iter() {
             print!("{}", pulse);
-        };
+        }
         print!(" ");
-    };
+    }
     println!();
 }
 
 fn main() {
-    let greeting = "Hello, world"
-        .to_string()
-        .to_morse_code();
-    
+    let greeting = "Hello, world".to_string().to_morse_code();
+
     print_morse_code(&greeting);
 }
 
-
-impl std::fmt::Display for Pulse {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Short => write!(f, "."), 
-            Self::Long => write!(f, "_"),
+impl MorseCode for String {
+    fn to_morse_code(&self) -> Message {
+        let mut ret: Message = Vec::with_capacity(self.capacity());
+        for c in self.chars() {
+            let pulse = match c.to_ascii_lowercase() {
+                'a' => vec![Pulse::Short, Pulse::Long],
+                'b' => vec![Pulse::Long, Pulse::Short, Pulse::Short, Pulse::Short],
+                'c' => vec![Pulse::Long, Pulse::Short, Pulse::Long, Pulse::Short],
+                'd' => vec![Pulse::Long, Pulse::Short, Pulse::Short],
+                'e' => vec![Pulse::Short],
+                'f' => vec![Pulse::Short, Pulse::Short, Pulse::Long, Pulse::Short],
+                'g' => vec![Pulse::Long, Pulse::Long, Pulse::Short],
+                'h' => vec![Pulse::Short, Pulse::Short, Pulse::Short, Pulse::Short],
+                'i' => vec![Pulse::Short, Pulse::Short],
+                'j' => vec![Pulse::Short, Pulse::Long, Pulse::Long, Pulse::Long],
+                'k' => vec![Pulse::Long, Pulse::Short, Pulse::Long],
+                'l' => vec![Pulse::Short, Pulse::Long, Pulse::Short, Pulse::Short],
+                'm' => vec![Pulse::Long, Pulse::Long],
+                'n' => vec![Pulse::Long, Pulse::Short],
+                'o' => vec![Pulse::Long, Pulse::Long, Pulse::Long],
+                'p' => vec![Pulse::Short, Pulse::Long, Pulse::Long, Pulse::Short],
+                'q' => vec![Pulse::Long, Pulse::Long, Pulse::Short, Pulse::Long],
+                'r' => vec![Pulse::Short, Pulse::Long, Pulse::Short],
+                's' => vec![Pulse::Short, Pulse::Short, Pulse::Short],
+                't' => vec![Pulse::Long],
+                'u' => vec![Pulse::Short, Pulse::Short, Pulse::Long],
+                'v' => vec![Pulse::Short, Pulse::Short, Pulse::Short, Pulse::Long],
+                'w' => vec![Pulse::Short, Pulse::Long, Pulse::Long],
+                'x' => vec![Pulse::Long, Pulse::Short, Pulse::Short, Pulse::Long],
+                'y' => vec![Pulse::Long, Pulse::Short, Pulse::Long, Pulse::Long],
+                'z' => vec![Pulse::Long, Pulse::Long, Pulse::Short, Pulse::Short],
+                '0' => vec![
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Long,
+                ],
+                '1' => vec![
+                    Pulse::Short,
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Long,
+                ],
+                '2' => vec![
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Long,
+                ],
+                '3' => vec![
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Long,
+                    Pulse::Long,
+                ],
+                '4' => vec![
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Long,
+                ],
+                '5' => vec![
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Short,
+                ],
+                '6' => vec![
+                    Pulse::Long,
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Short,
+                ],
+                '7' => vec![
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Short,
+                    Pulse::Short,
+                    Pulse::Short,
+                ],
+                '8' => vec![
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Short,
+                    Pulse::Short,
+                ],
+                '9' => vec![
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Long,
+                    Pulse::Short,
+                ],
+                _ => continue,
+            };
+            ret.push(pulse);
         }
+        ret
     }
 }
+
+//impl std::fmt::Display for Pulse {
+//    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//        match self {
+//            Self::Short => write!(f, "."),
+//            Self::Long => write!(f, "_"),
+//        }
+//    }
+//}
 
 #[test]
 fn hello_world() {
